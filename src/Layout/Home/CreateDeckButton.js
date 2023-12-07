ea90f4382a1f57f92b2c9499ce32cadf2f256015
Cain Bailey <cainbailey1993@gmail.com>
2023-12-06 16:14:06 -0800

    Changed styling on Home screen

--- src/Layout/Home/CreateDeckButton.js
+++ src/Layout/Home/CreateDeckButton.js
@@ -4,7 +4,7 @@ import {useHistory} from 'react-router-dom';
 function CreateDeckButton() {
     const history = useHistory();
     return (
-        <button className="btn btn-secondary" onClick={() => history.push("/decks/new")}>
+        <button className="btn btn-success" onClick={() => history.push("/decks/new")}>
             <i className="bi bi-plus"></i>Create Deck
         </button>
     )
